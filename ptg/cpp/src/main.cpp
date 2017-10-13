75abd4624fa6d0bf7a9a2eebf006a7ba12c48e52
jcbecker <joaocarlosbecker666@gmail.com>
2017-10-13 09:08:00 -0300

    rename shader loader file

--- ptg/cpp/src/main.cpp
+++ ptg/cpp/src/main.cpp
@@ -11,7 +11,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include <shader.cpp>
+#include <shader.hpp>
 #include <camera.hpp>
 //Global settings
 //initial screen size
