0cef0dd3c47426c46ccdcf97fc52bcfb9fedbc9a
jcbecker <joaocarlosbecker666@gmail.com>
2017-11-14 10:29:54 -0200

    removing cubes

--- ptg/cpp/src/main.cpp
+++ ptg/cpp/src/main.cpp
@@ -52,102 +52,6 @@ int main(){
     
     Shader terrainProgram("shader/terrain.vs", "shader/terrain.fs"); // you can name your shader files however you like
     
-    // set up vertex data (and buffer(s)) and configure vertex attributes
-    // ------------------------------------------------------------------
-    float vertices[] = {
-        // positions         // colors
-        0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 1.0f,// top right
-        0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 1.0f,// bottom right
-        -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f,// bottom left
-        -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f,// top left
-        
-        0.5f,  0.5f, 0.5f,  0.0f, 0.0f, 0.0f,// top right
-        0.5f, -0.5f, 0.5f,  0.0f, 0.0f, 0.0f,// bottom right
-        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 0.0f,// bottom left
-        -0.5f,  0.5f, 0.5f, 0.0f, 0.0f, 0.0f,// top left
-        
-        -0.5f,  0.5f, -0.5f, 0.0f, 0.0f, 1.0f,// top left
-        -0.5f,  0.5f, 0.5f, 0.0f, 0.0f, 1.0f,// top left
-        0.5f,  0.5f, 0.5f,  0.0f, 0.0f, 1.0f,// top right
-        0.5f,  0.5f, -0.5f,  0.0f, 0.0f, 1.0f,// top right
-        
-        -0.5f,  -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,// bottom left
-        -0.5f,  -0.5f, 0.5f, 1.0f, 0.0f, 0.0f,// bottom left
-        0.5f,  -0.5f, 0.5f,  1.0f, 0.0f, 0.0f,// bottom right
-        0.5f,  -0.5f, -0.5f,  1.0f, 0.0f, 0.0f,// bottom right
-        
-        0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 0.0f,// top right
-        0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.0f,// bottom right
-        0.5f, -0.5f, 0.5f,  0.0f, 1.0f, 0.0f,// bottom right
-        0.5f, 0.5f, 0.5f,  0.0f, 1.0f, 0.0f,// bottom right
-        
-        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 0.0f,// top right
-        -0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 0.0f,// bottom right
-        -0.5f, -0.5f, 0.5f,  1.0f, 1.0f, 0.0f,// bottom right
-        -0.5f, 0.5f, 0.5f,  1.0f, 1.0f, 0.0f// bottom right
-        
-    };
-    unsigned int indices[] = {  // note that we start from 0!
-        0, 3, 2,  // first Triangle
-        1, 2, 0,   // second Triangle
-        
-        0+4, 3+4, 2+4,  // first Triangle
-        1+4, 2+4, 0+4,   // second Triangle
-        
-        0+8, 3+8, 2+8,  // first Triangle
-        1+8, 2+8, 0+8,   // second Triangle
-        
-        0+(4*3), 3+(4*3), 2+(4*3),  // first Triangle
-        1+(4*3), 2+(4*3), 0+(4*3),   // second Triangle
-        
-        0+(4*4), 3+(4*4), 2+(4*4),  // first Triangle
-        1+(4*4), 2+(4*4), 0+(4*4),   // second Triangle
-        
-        0+(4*5), 3+(4*5), 2+(4*5),  // first Triangle
-        1+(4*5), 2+(4*5), 0+(4*5)  // second Triangle
-    };
-    // world space positions of our cubes
-    glm::vec3 cubePositions[] = {
-        glm::vec3( 0.0f,  0.0f,  0.0f),
-        glm::vec3( 2.0f,  5.0f, -15.0f),
-        glm::vec3(-1.5f, -2.2f, -2.5f),
-        glm::vec3(-3.8f, -2.0f, -12.3f),
-        glm::vec3( 2.4f, -0.4f, -3.5f),
-        glm::vec3(-1.7f,  3.0f, -7.5f),
-        glm::vec3( 1.3f, -2.0f, -2.5f),
-        glm::vec3( 1.5f,  2.0f, -2.5f),
-        glm::vec3( 1.5f,  0.2f, -1.5f),
-        glm::vec3(-1.3f,  1.0f, -1.5f)
-    };
-
-    unsigned int VBO, VAO, EBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-    
-    
-    // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    // color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
-    glEnableVertexAttribArray(1);
-
-    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-    glBindVertexArray(0);
-    
     
     float myAxes[] = {
         1000.0f,  0.0f, 0.0f,  1.0f, 0.0f, 0.0f,
@@ -175,8 +79,6 @@ int main(){
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
     
-    printf("VBO: %u   VAO: %u   EBO: %u   aVBO: %u   aVAO: %u\n", VBO, VAO, EBO, aVBO, aVAO);
-    printf("vertices: %d   indices: %d    myAxes: %d\n", sizeof(vertices), 12*3*sizeof(unsigned int), 6*6*sizeof(float));
     
     std::vector<float> gridstf;
     gridstf.reserve(1024*1024*6);
@@ -265,7 +167,6 @@ int main(){
     glViewport(0, 0, ISCR_W, ISCR_H);
     
     //creating variables
-    
     int cscr_w=ISCR_W, cscr_h=ISCR_H;
     float deltaTime = 0.0f;	// Time between current frame and last frame
     float lastFrame = 0.0f; // Time of last frame
@@ -303,7 +204,7 @@ int main(){
         //render
         //clear the collor buffer
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // also clear the depth buffer now!
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);//Clear the buffers
         
         
         //active the shader and bind uniforms
@@ -312,7 +213,7 @@ int main(){
         // create transformations
         view  = cam.getViewMatrix();
         glfwGetWindowSize(window, &cscr_w, &cscr_h);//essa função é threadsafe
-        projection = glm::perspective(glm::radians(cam.zoomC), (float)cscr_w / (float)cscr_h, 0.1f, 100.0f);
+        projection = glm::perspective(glm::radians(cam.zoomC), (float)cscr_w / (float)cscr_h, 0.1f, 1000.0f);
         // retrieve the matrix uniform locations
         // note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
         terrainProgram.setMat4("projection", projection);
@@ -320,25 +221,6 @@ int main(){
         
         
         //render container
-        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        for(unsigned int i = 0; i < 10; i++){
-            
-            
-            model = glm::mat4(1.0f);
-            model = glm::translate(model, cubePositions[i]);
-            float angle = 30.0f * i;
-            if (i%2){
-                model = glm::rotate(model, glm::radians((float)glfwGetTime()*100), glm::vec3(0.0f, 0.0f, 1.0f));
-            }else{
-                model = glm::rotate(model, glm::radians((float)glfwGetTime()*100), glm::vec3(-1.0f, 0.0f, 0.0f));
-            }
-            
-            terrainProgram.setMat4("model", model);
-            
-            glDrawElements(GL_TRIANGLES, 6*6, GL_UNSIGNED_INT, 0);
-            //glDrawArrays(GL_TRIANGLE_FAN, 0, 36);
-        }
-        
         glBindVertexArray(aVAO);
         model = glm::mat4(1.0f);
         terrainProgram.setMat4("model", model);
@@ -353,7 +235,6 @@ int main(){
         //glDrawArrays(GL_POINTS, 0, 1024*1024);
         //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
         glDrawElements(GL_TRIANGLES, 1023*1023*6, GL_UNSIGNED_INT, 0);
-        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
         
         
         //glDrawElements(GL_TRIANGLES, 6*6, GL_UNSIGNED_INT, 0);
@@ -366,9 +247,6 @@ int main(){
     
     printf("Free memory space...\n");
     //de-allocate all resources once they've outlived their purpose
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &EBO);
     glDeleteVertexArrays(1, &grVAO);
     glDeleteBuffers(1, &grVBO);
     glDeleteBuffers(1, &grEBO);
@@ -392,7 +270,6 @@ void framebufferSizeCallback(GLFWwindow* window, int width, int height){//resize
     // make sure the viewport matches the new window dimensions; note that width and 
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
-    printf("Screen:(%d, %d)\n",width, height);
 }
 
 void processInputHoldingKey(GLFWwindow *window, float deltaTime){
